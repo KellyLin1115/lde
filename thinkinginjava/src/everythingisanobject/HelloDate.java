27c0f4f57aa065b38c804ab6ace4a72b5ea901f5
Kelly Lin <xiaoyumumu911@163.com>
2018-04-14 16:15:08 +0800

    Add HelloDate in chapter everything is an object

---
+++ thinkinginjava/src/everythingisanobject/HelloDate.java
@@ -0,0 +1,13 @@
+package everythingisanobject;
+
+import java.util.Date;
+
+/**
+ * Created by donger on 4/14/18.
+ */
+public class HelloDate {
+    public static void main(String[] args){
+        System.out.println("Hello, it's ");
+        System.out.println(new Date());
+    }
+}
